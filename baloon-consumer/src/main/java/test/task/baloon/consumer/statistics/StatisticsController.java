181b20617b74adf7bcabeeea430944c7de1e6d7e
Igor Faust <ext.igor.faust@hq.tp.nil>
2020-11-27 13:55:59 +0200

    add controller to get history

--- baloon-consumer/src/main/java/test/task/baloon/consumer/statistics/StatisticsController.java
+++ baloon-consumer/src/main/java/test/task/baloon/consumer/statistics/StatisticsController.java
@@ -1,4 +1,31 @@
 package test.task.baloon.consumer.statistics;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import test.task.baloon.consumer.weather.WeatherRepository;
+import test.task.baloon.consumer.weather.model.Weather;
+
+@RestController
 public class StatisticsController {
+	
+	private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm");
+	
+	@Autowired
+	private WeatherRepository weatherRepository;
+	
+	@GetMapping("/weather-history")
+	List<Weather> getWeatherList(@RequestParam String dateFrom, @RequestParam String dateTo) {
+		return weatherRepository.findWeatherByTimeStampBetween(
+				LocalDateTime.parse(dateFrom, FORMATTER),
+				LocalDateTime.parse(dateTo, FORMATTER));
+	}
+
 }
--- baloon-consumer/src/main/java/test/task/baloon/consumer/weather/WeatherRepository.java
+++ baloon-consumer/src/main/java/test/task/baloon/consumer/weather/WeatherRepository.java
@@ -1,8 +1,12 @@
 package test.task.baloon.consumer.weather;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import test.task.baloon.consumer.weather.model.Weather;
 
 public interface WeatherRepository extends CrudRepository<Weather, Long> {
+	List<Weather> findWeatherByTimeStampBetween(LocalDateTime dateFrom, LocalDateTime dateTo);
 }
